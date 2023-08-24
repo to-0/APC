479c00932cb6155153fb46713d43319baf6e7f70
Adam Berenik <adam.berenik@eset.com>
2023-08-24 09:27:47 +0200

    refactoring in monitoring_admin.js

--- cppseminar/presentation/wwwroot/js/monitoring_admin.js
+++ cppseminar/presentation/wwwroot/js/monitoring_admin.js
@@ -1,6 +1,3 @@
-// setInterval(()=>{
-//     var response = fetch("http://localhost:8080/monitoring/getUsers")
-// },5000)
 const connection = new signalR.HubConnectionBuilder()
     .withUrl("/monitor")
     .configureLogging(signalR.LogLevel.Information)
@@ -10,7 +7,6 @@ async function start() {
     try {
         await connection.start();
         console.log("SignalR Connected.");
-        setInterval(invokeSendMessage, 1000);
     }
     catch (err) {
         console.log(err);
@@ -25,49 +21,47 @@ connection.onclose(async () => {
 
 // Define the ReceiveMessage method so that it can be triggered from the Hub
 connection.on("ReceiveUsers", (users) => {
-    try{
+    try {
         users = JSON.parse(users);
-    console.log(users);
-    users.forEach(user => {
-        const row = document.getElementById(user.UserEmail)
-        const tbl = document.getElementById("userLogs");
-        const dateNow = Date.now()
-        const timestamp = new Date(user.Timestamp) 
-        if (row === null){
-            const temp = `<b><tr id=${user.UserEmail}><td>${user.UserEmail}</td><td>${Math.floor((dateNow - timestamp) / 1000)}</td></tr></b>`;
-            tbl.innerHTML += temp;
-        }
-        else{
-            row.innerHTML = `<td> 
-                            ${user.UserEmail}
-                            </td>
-                            <td>
-                            ${Math.floor((dateNow - timestamp) / 1000)}
-                            </td>`
-        }
-        console.log(user.UserEmail);
-        console.log(user.Timestamp);
-    })
+        // console.log(users);
+        users.forEach(user => {
+            const row = document.getElementById(user.UserEmail)
+            const tbl = document.getElementById("userLogs");
+            const dateNow = Date.now()
+            const timestamp = new Date(user.Timestamp) 
+            if (row === null) {
+                const temp = `<b><tr id=${user.UserEmail}><td>${user.UserEmail}</td><td>${Math.floor((dateNow - timestamp) / 1000)}</td></tr></b>`;
+                tbl.innerHTML += temp;
+            }
+            else {
+                row.innerHTML = `<td> 
+                                ${user.UserEmail}
+                                </td>
+                                <td>
+                                ${Math.floor((dateNow - timestamp) / 1000)}
+                                </td>`
+            }
+        })
     }
     catch (exception){
         console.log(exception);
     }
-    
 });
 
-async function invokeSendMessage() {
+async function invokeGetConnectedUsersRecentAsync() {
     // Invoke SendMessage on the Hub
     try {
         await connection.invoke("GetConnectedUsersRecentAsync");
     } catch (err) {
         console.error(err);
     }
-    return false;
 }
 
 async function main() {
     // Start the connection.
     await start();
+
+    setInterval(invokeGetConnectedUsersRecentAsync, 1000);
 }
 
 main();
