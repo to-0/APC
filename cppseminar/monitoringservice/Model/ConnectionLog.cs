9296fdb247b53d6f2ee2c8799e531be6ea5b9fbe
to-0 <tomas1.kalny@gmail.com>
2023-08-24 11:42:33 +0200

    Moved last seen calculation to presentation service

--- cppseminar/monitoringservice/Model/ConnectionLog.cs
+++ cppseminar/monitoringservice/Model/ConnectionLog.cs
@@ -7,13 +7,13 @@ namespace monitoringservice.Model
 {
     public class ConnectionLog
     {
-        public ConnectionLog(string email, string timestamp){
+        public ConnectionLog(string email, DateTime timestamp){
             UserEmail = email;
             Timestamp = timestamp;
         }
         public ConnectionLog(){
         }
         public string UserEmail { get; set; }
-        public string Timestamp { get; set; }
+        public DateTime Timestamp { get; set; }
     }
 }
